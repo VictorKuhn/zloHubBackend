467518cae82972c0ec0b10e4f1e474472f41e342
Victor Kuhn <victorkuhn.dev@gmail.com>
2024-11-28 15:36:38 -0300

    Correção de segurança e testes indicados pelo SonarQube.

--- src/test/java/com/zlohub/zlohub/ZlohubBackApplicationTests.java
+++ src/test/java/com/zlohub/zlohub/ZlohubBackApplicationTests.java
@@ -3,15 +3,19 @@ package com.zlohub.zlohub;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 @SpringBootTest
 class ZlohubBackApplicationTests {
 
     @Test
     void contextLoads() {
+        // Este método está vazio porque sua única função é verificar se o contexto do Spring Boot carrega corretamente.
     }
 
     @Test
     void mainMethodRunsSuccessfully() {
-        ZlohubBackApplication.main(new String[] {});
+        // Assertion adicionada para garantir que nenhuma exceção é lançada
+        assertDoesNotThrow(() -> ZlohubBackApplication.main(new String[] {}));
     }
 }
